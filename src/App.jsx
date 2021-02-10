a66aff51b2828da5ab536e3e15d91935b05c31d7
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2021-02-10 02:45:32 -0500

    updated firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,167 +1,49 @@
-import React from 'react';
-import {firebase} from './firebase'
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Admin from './components/Admin'
 
-function App() {
-
-  const [tareas, setTareas] = React.useState([])
-  const [tarea, setTarea] = React.useState('')
-  const [modoEdicion, setModoEdicion] = React.useState(false)
-  const [id, setId] = React.useState('')
+import {auth } from './firebase'
 
 
-  React.useEffect(() => {
+const App = () => {
 
-    const obtenerDatos = async () => {
+    const [firebaseUser, setFirebaseUser] = React.useState(false)
 
-      try {
-
-        const db = firebase.firestore()
-        const data = await db.collection('tareas').get()
-        const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-        console.log(arrayData)
-        setTareas(arrayData)
-        
-      } catch (error) {
-        console.log(error)
-      }
-
-    }
-
-    obtenerDatos()
-
-  }, [])
-
-  const agregar = async (e) => {
-    e.preventDefault()
-
-    if(!tarea.trim()){
-      console.log('está vacio')
-      return
-    }
-
-    try {
-
-      const db = firebase.firestore()
-      const nuevaTarea = {
-        name: tarea,
-        fecha: Date.now()
-      }
-      const data = await db.collection('tareas').add(nuevaTarea)
-
-      setTareas([
-        ...tareas,
-        {...nuevaTarea, id: data.id}
-      ])
-
-      setTarea('')
-      
-    } catch (error) {
-      console.log(error)
-    }
-
-    console.log(tarea)
-  }
-
-  const eliminar = async (id) => {
-    try {
-      
-      const db = firebase.firestore()
-      await db.collection('tareas').doc(id).delete()
-
-      const arrayFiltrado = tareas.filter(item => item.id !== id)
-      setTareas(arrayFiltrado)
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const activarEdicion = (item) => {
-    setModoEdicion(true)
-    setTarea(item.name)
-    setId(item.id)
-  }
-
-  const editar = async (e) => {
-    e.preventDefault()
-    if(!tarea.trim()){
-      console.log('vacio')
-      return
-    }
-    try {
-      
-      const db = firebase.firestore()
-      await db.collection('tareas').doc(id).update({
-        name: tarea
-      })
-      const arrayEditado = tareas.map(item => (
-        item.id === id ? {id: item.id, fecha: item.fecha, name: tarea} : item
-      ))
-      setTareas(arrayEditado)
-      setModoEdicion(false)
-      setTarea('')
-      setId('')
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  return (
-    <div className="container mt-3">
-      <div className="row">
-        <div className="col-md-6">
-          <h3>Lista de tareas</h3>
-          <ul className="list-group">
-            {
-              tareas.map(item => (
-                <li className="list-group-item" key={item.id}>
-                  {item.name}
-                  <button 
-                    className="btn btn-danger btn-sm float-right"
-                    onClick={() => eliminar(item.id)}
-                  >
-                    Eliminar
-                  </button>
-                  <button 
-                    className="btn btn-warning btn-sm float-right mr-2"
-                    onClick={() => activarEdicion(item)}
-                  >
-                    Editar
-                  </button>
-                </li>
-              ))
+    React.useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            console.log(user)
+            if(user){
+                setFirebaseUser(user)
+            }else{
+                setFirebaseUser(null)
             }
-          </ul>
-        </div>
-        <div className="col-md-6">
-          <h3>
-            {
-              modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'
-            }
-          </h3>
-          <form onSubmit={modoEdicion ? editar : agregar}>
-            <input 
-              type="text"
-              placeholder="Ingrese tarea"
-              className="form-control mb-2"
-              onChange={e => setTarea(e.target.value)}
-              value={tarea}
-            />
-            <button 
-              className={
-                modoEdicion ? 'btn btn-warning btn-block' : 'btn btn-dark btn-block'
-              }
-              type="submit"
-            >
-              {
-                modoEdicion ? 'Editar' : 'Agregar'
-              }
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+        })
+    }, [])
+
+    return firebaseUser !== false ? (
+        <Router>
+            <div className="container">
+                <Navbar firebaseUser={firebaseUser}/>
+                <Switch>
+                    <Route path="/" exact>
+                    {
+                        firebaseUser && <p className="mt-1">{firebaseUser.email}</p>
+                    }
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/admin">
+                        <Admin />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    ): (
+        <div>Cargando...</div>
+    )
 }
 
-export default App;
+export default App
